82153293c1dd951a74a2115b8d0a409d5b594381
cedrick macatangay <cedrickmacatangay@gmail.com>
2012-01-09 20:14:55 +0800

    update about_text

---
+++ about_text.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+<title>Locators R Us</title>
+<link rel="shortcut icon" href="templates/images/favicon.ico" type="image/x-icon" />
+<link rel="stylesheet" href="library/ui-lightness/jquery-ui.css" />
+<script type="text/javascript" src="library/jquery.js"></script>
+<script type="text/javascript" src="library/jquery-ui.min.js"></script>
+<script type="text/javascript">
+//$(document).ready(function(){
+
+	
+	
+	//$("#but").click(function(){
+	
+		//$(function() {
+			// a workaround for a flaw in the demo system (http://dev.jqueryui.com/ticket/4375), ignore!
+			//$( "#box" ).dialog( "destroy" );
+		
+			//$( "#box" ).dialog({
+				//height: 140,
+				//modal: true
+			//});
+		//});
+	//});
+//});
+
+
+</script>
+</head>
+<body>
+	
+<?php include ("templates/style.css"); ?>
+<?php //include ("templates/template.php"); ?>
+<!--  <div class="liner">
+</div>-->
+	
+
+<div class="container_about">
+<table cellspacing="0" cellpadding="0">
+	<tr>
+		<td>
+			<img class="about" src="templates/images/about_content_text.png"  alt="about"/>
+		</td>
+	</tr>
+
+</table>
+</div>
+<?php //include ("templates/footer.php"); ?>
+
+</body>
+</html>
+
