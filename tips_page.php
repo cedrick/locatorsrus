b8948b3a55cbd1a9f4fda7cec086ef27aed0958a
cedrick macatangay <cedrickmacatangay@gmail.com>
2012-01-09 20:23:53 +0800

    update tips_page

---
+++ tips_page.php
@@ -0,0 +1,64 @@
+<html>
+<head>
+<title>Locators R Us</title>
+<link rel="shortcut icon" href="templates/images/favicon.ico" type="image/x-icon" />
+<link rel="stylesheet" href="library/ui-lightness/jquery-ui.css" />
+<script type="text/javascript" src="library/jquery.js"></script>
+<script type="text/javascript" src="library/jquery-ui.min.js"></script>
+<script type="text/javascript">
+//$(document).ready(function(){
+
+	
+	
+	//$("#but").click(function(){
+	
+		//$(function() {
+			// a workaround for a flaw in the demo system (http://dev.jqueryui.com/ticket/4375), ignore!
+			//$( "#box" ).dialog( "destroy" );
+		
+			//$( "#box" ).dialog({
+				//height: 140,
+				//modal: true
+			//});
+		//});
+	//});
+//});
+
+
+</script>
+</head>
+<body>
+	
+<?php include ("templates/style.css"); ?>
+<?php //include ("templates/template.php"); ?>
+<!--  <div class="liner">
+</div>-->
+<center>
+<div class = "container">
+<table cellspacing="0" cellpadding="0">
+	<tr>
+		<td>
+			<img class="back" src="templates/images/locators_back.png" height="876" alt="locators_back"/>
+			<div class="content" align="center">
+		
+				<div class="quick_links">
+				<?php include ("templates/navigation.php"); ?>
+				</div>
+				<div class="tips_content">
+					<iframe src="tips.php" width="750" height="300" frameborder="0" class="fcontent">
+						
+						
+						
+					</iframe>
+				</div>
+				
+			</div>
+		</td>
+	</tr>
+</table>
+</div>
+<?php //include ("templates/footer.php"); ?>
+</center>
+</body>
+</html>
+
