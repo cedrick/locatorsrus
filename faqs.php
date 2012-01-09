8069d1d40a284ae6c68988943d3560b6e58743d3
cedrick macatangay <cedrickmacatangay@gmail.com>
2012-01-09 20:21:00 +0800

    update faqs

---
+++ faqs.php
@@ -0,0 +1,294 @@
+<html>
+<head>
+<title>Locators R Us</title>
+<link rel="shortcut icon" href="templates/images/favicon.ico" type="image/x-icon" />
+<link rel="stylesheet" href="library/ui-lightness/jquery-ui.css" />
+<script type="text/javascript" src="library/jquery.js"></script>
+<script type="text/javascript" src="library/jquery-ui.min.js"></script>
+<script type="text/javascript">
+//$(document).ready(function(){
+
+	
+	
+	//$("#but").click(function(){
+	
+		//$(function() {
+			// a workaround for a flaw in the demo system (http://dev.jqueryui.com/ticket/4375), ignore!
+			//$( "#box" ).dialog( "destroy" );
+		
+			//$( "#box" ).dialog({
+				//height: 140,
+				//modal: true
+			//});
+		//});
+	//});
+//});
+
+
+</script>
+</head>
+<body>
+	
+<?php include ("templates/style.css"); ?>
+<?php //include ("templates/template.php"); ?>
+<!--  <div class="liner">
+</div>-->
+	
+
+<div class="container_faq">
+<table>
+	<tr>
+		<td>
+			<font color="RED" size="5">Frequently Asked Questions</font>
+		</td>
+	</tr>
+	<tr>
+		
+	</tr>
+	<tr>
+		
+	</tr>
+	<tr>
+		
+	</tr>
+	<tr>
+		<td>
+			<b>Q) Why use a locating service?</b>
+		</td>
+	</tr>
+	<tr>
+	
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A) Location is the most important part of any vending machine business. Yes, for a vending machine 
+		</td>
+	</tr>
+	<tr>
+	
+		<td>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;business to be successful you must firstly have secure locations that will suit your vending machine 		
+		</td>
+	</tr>
+	<tr>
+
+		<td>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;type, size and capacity. And if you are not comfortable calling people or dealing face to face with the 	
+		</td>
+	</tr>
+	<tr>
+	
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;business owners or store managers then it's time for you to hire a locating service. Locators R Us is 
+		</td>
+	</tr>
+	<tr>
+
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;team of savvy and skilled negotiators who will give you the best deals.
+		</td>
+	</tr>
+	<tr></tr>
+	<tr></tr>
+	<tr>
+		<td>
+			<b>Q) How long have you been locating for vending machines?</b>
+		</td>
+	</tr>
+	<tr>
+	
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A) We have been locating since 2006. And we have worked for different vending locators in US and Canada.
+		</td>
+	</tr>
+	<tr></tr>
+	<tr></tr>
+	<tr>
+		<td>
+			<b>Q) Can you give me references?</b>
+		</td>
+	</tr>
+	<tr>
+
+		<td>
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A)  As much as we want to give you references to answer your doubts, we cannot do that because we have a
+		</td>
+	</tr>
+	<tr>
+	
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we have a non disclosure agreement with our previous clients. We prefer to show you how we work and 
+		</td>
+	</tr>
+	<tr>
+	
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;how good we are.
+		</td>
+	</tr>
+	<tr></tr>
+	<tr></tr>
+	<tr>
+		<td>
+			<b>Q) How good are you in finding vending locations?</b>
+		</td>
+	</tr>
+	<tr>
+	
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A) We have gotten thousands of vending locations for hundreds of vending machine companies. 
+		</td>
+	</tr>
+	<tr>
+	
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We don’t know but maybe we have found locations for you before.
+		</td>
+	</tr>
+	<tr></tr>
+	<tr></tr>
+	<tr>
+		<td>
+			<b>Q) What assurance do I get on being satisfied with my vending locations?</b>
+		</td>
+	</tr>
+	<tr>
+	
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A) You, our valued customer, have the last say in every location we get for you. If, in any way,
+		</td>
+	</tr>
+	<tr>
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;you are not satisfied with the location you only pay for the ones you approve. 
+		</td>
+	</tr>
+	<tr>
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We also welcome any “Wish List” of the business you want your machines to be put 
+		</td>
+	</tr>
+	<tr>
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;into and we will exert our efforts to succeed in your target area.
+		</td>
+	</tr>
+	<tr></tr>
+	<tr></tr>
+	<tr>
+		<td>
+			<b>Q) What is your warranty or relocation policy?</b>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A) Locators R Us provides Warranty on all your machines and locations because we aim
+		</td>
+	</tr>
+	<tr>
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;to provide you with genuine guarantee and satisfaction.
+		</td>
+	</tr>
+	<tr></tr>
+	<tr></tr>
+	<tr>
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font color="RED">We have a 60 day guarantee where in we replace machines that are kicked out the business absolutely</font>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <font color="RED">by FREE! We also have a LIFETIME RELOCATION DISCOUNT for your machines.<font color="RED">
+		</td>
+	</tr>
+	<tr></tr>
+	<tr></tr>
+	<tr>
+		<td>
+			<b>Q) What is Lifetime Relocation Discount?</b>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A) If in any case, for any reason, you want to relocate your machine after 30 days of placement.
+		</td>
+	</tr>
+	<tr>
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We will look for another location for your machine for only $15.00
+		</td>
+	</tr>
+	<tr></tr>
+	<tr></tr>
+	<tr>
+		<td>
+			<b>Q) Can you help me deliver?</b>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A) As much as we want too, we cannot do that because we are located in the Philippines. 
+		</td>
+	</tr>
+	<tr>
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We find locations over the phone only.
+		</td>
+	</tr>
+	<tr></tr>
+	<tr></tr>
+	<tr>
+		<td>
+			<b>Q) Why are your services inexpensive?</b>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A) Since we ourselves find the locations for you, we have low over head expenses. 
+		</td>
+	</tr>
+	<tr>
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;And we want to share our savings to by giving you a reasonable price.
+		</td>
+	</tr>
+	<tr></tr>
+	<tr></tr>
+	<tr>
+		<td>
+			<b>Q) Why not do all vending types?</b>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A) We would not like to compromise our services and your satisfaction. That's why 
+		</td>
+	</tr>
+	<tr>
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we only do certain types of machines that we know we can handle.
+		</td>
+	</tr>
+	<tr></tr>
+	<tr></tr>
+	<tr>
+		<td>
+			<b>Q) Do you locate in other countries aside from US?</b>
+		</td>
+	</tr>
+	<tr>
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;A) Yes, we also locate in Canada, Australia and UK. US and Canada has the same rate.
+		</td>
+	</tr>
+	<tr>
+		<td>
+			&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; For Australia and UK, email us for pricing.
+		</td>
+	</tr>
+</table>
+</div>
+<?php //include ("templates/footer.php"); ?>
+
+</body>
+</html>
+
