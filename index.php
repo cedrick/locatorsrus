c6b1fcc5a18e36edcb6ce9808efc1c9de21fea57
cedrick macatangay <cedrickmacatangay@gmail.com>
2012-01-09 20:21:45 +0800

    update index

--- index.php
+++ index.php
@@ -35,36 +35,30 @@
 </div>-->
 	
 <center>
-<div class="content">
-<div class="quick_links">
-<?php include ("templates/navigation.php"); ?>
+<div class = "container">
+<table cellspacing="0" cellpadding="0">
+	<tr>
+		<td>
+			<img class="back" src="templates/images/locators_back.png" height="876" alt="locators_back"/>
+			<div class="content" >
+		
+				<div class="quick_links">
+				<?php include ("templates/navigation.php"); ?>
+				</div>
+				<div class="sub_content_about">
+					<iframe src="home_text.php" width="750" height="300" frameborder="0" class="fcontent">
+						
+						
+						
+					</iframe>
+				</div>
+			</div>
+		</td>
+	</tr>
+
+</table>
 </div>
-<div class="sub_content" align="left">
-<font color="RED" size="5"><b>Locators R Us</b></font> 
-<font size="3">provides vending machine locating service to vending machine owners and companies in the US and Canada. Our service includes locating profitable businesses to place any type of vending machine - from candy machines to snack boxes. We have experienced sales staff who knows exactly what you need and what you're trying to achieve. If you're looking for a vending machine locator who can provide quality locations and help you succeed in the business then we are definitely the one to get.</font>
-<br><br>
-<font color="RED" size="5">Why should you choose Locators R Us?</font>
-<font size="3">
-	<br>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font size="6">.</font>Our team knows well paying vending locations because WE have been locating since 2006. By 
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;saying WE, it means we don’t have people work for us. WE ourselves do the work. 
-	<br>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font size="6">.</font>We will represent your company well and all you will ever do is to place the machine.<br>
-	
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font size="6">.</font>We will give you the best. Once you get a location it’s almost 100% guaranteed to get placed
-	<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;and less likely to be rejected.<br>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font size="6">.</font>If you’re new to the vending business, our staff can help you devise strategies to get your 
-	<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;machines placed right away. We know just what store owners and managers want for them to say 
-	<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;yes.<br>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font size="6">.</font>You, our valued customer, have the last say in every location we get for you. If, in any way,
-	<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;you are not satisfied with the location you only pay for the ones you approve.<br>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font size="6">.</font>All of our quality services are for a very reasonable price. Other locating services can get a little 
-	<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;pricey, but with Locators R Us you will get maximum profit out of your vending machines because 
-	<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;we keep it as affordable and profitable to you as we possibly can.
-</font>
-</div>
-</div>
-<?php include ("templates/footer.php"); ?>
+<?php //include ("templates/footer.php"); ?>
 </center>
 </body>
 </html>
