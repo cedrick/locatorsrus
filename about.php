e2848c7caa4ebbd1126e25bee855e156205ac7c7
cedrick macatangay <cedrickmacatangay@gmail.com>
2012-01-09 20:15:44 +0800

    update about

--- about.php
+++ about.php
@@ -33,39 +33,30 @@
 <?php //include ("templates/template.php"); ?>
 <!--  <div class="liner">
 </div>-->
-	
 <center>
-<div class="content">
-<div class="quick_links">
-<?php include ("templates/navigation.php"); ?>
+<div class = "container">
+<table cellspacing="0" cellpadding="0">
+	<tr>
+		<td>
+			<img class="back" src="templates/images/locators_back.png" height="876" alt="locators_back"/>
+			<div class="content" align="center">
+		
+				<div class="quick_links">
+				<?php include ("templates/navigation.php"); ?>
+				</div>
+				<div class="sub_content_about">
+					<iframe src="about_text.php" width="750" height="300" frameborder="0" class="fcontent">
+						
+						
+						
+					</iframe>
+				</div>
+			</div>
+		</td>
+	</tr>
+</table>
 </div>
-<div class="sub_content_about" align="left">
-<font size="3"> 
-&nbsp;&nbsp;&nbsp;<font color="RED" size="5"><b>A team</b></font> of locators started out in the summer of 2006 in Cebu City, Philippines. Our team includes experienced sales persons who have worked for numerous vending machine owners and manufacturers for years and have provided them hundreds of quality locations all over the US and Canada. Not only do we get you these locations but also make it a point that you get maximum profitability. We contact a wide range of potential businesses that has good foot traffic to ensure that you get the most out of your vending machines.
-<br><br>
-&nbsp;&nbsp;&nbsp;Unlike other locating services out there, our sales staff specifically specialize in vending machine location. We understand vending machines and where they're likely to have good turnouts. This guarantees that you get the best fit for any type of vending machine you have.
-</font>
-<br><br>
-<font color="RED" size="5">Our Aim</font>
-<font size="3">
-	<br>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font size="6">.</font>We have a staff of competent agents dedicated on vending machine location who will consistently work with you throughout 
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;the time that you are with us.
-	<br>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font size="6">.</font>We'd like you to give us as much information about your business. Let us know your what you are about, your goals and your 
-	<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;concerns. Basically, let us know everything. :)
-	<br>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font size="6">.</font>We can assure you that we won't stop working until we give you a good location for your vending machines.
-	<br>
-<font color="RED" size="5">Locating</font>
-	<br>
-	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<font size="6">.</font>We know what type of machines will work for certain types of businesses. Our main concern is for you to get the best 
-	<br>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;locations available.
-	
-</font>
-</div>
-</div>
-<?php include ("templates/footer.php"); ?>
+<?php //include ("templates/footer.php"); ?>
 </center>
 </body>
 </html>
