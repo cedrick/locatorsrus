ad9531da3efeababb0505d07d86047ac592b93be
cedrick macatangay <cedrickmacatangay@gmail.com>
2012-01-09 20:23:13 +0800

    update tips

---
+++ tips.php
@@ -0,0 +1,115 @@
+<html>
+<head>
+<title>Locators R Us</title>
+<link rel="shortcut icon" href="templates/images/favicon.ico" type="image/x-icon" />
+<link rel="stylesheet" href="library/ui-lightness/jquery-ui.css" />
+<script type="text/javascript" src="library/jquery.js"></script>
+<script type="text/javascript" src="library/jquery-ui.min.js"></script>
+<script type="text/javascript">
+//$(document).ready(function(){
+
+	
+	
+	//$("#but").click(function(){
+	
+		//$(function() {
+			// a workaround for a flaw in the demo system (http://dev.jqueryui.com/ticket/4375), ignore!
+			//$( "#box" ).dialog( "destroy" );
+		
+			//$( "#box" ).dialog({
+				//height: 140,
+				//modal: true
+			//});
+		//});
+	//});
+//});
+
+
+</script>
+</head>
+<body>
+	
+<?php include ("templates/style.css"); ?>
+<?php //include ("templates/template.php"); ?>
+<!--  <div class="liner">
+</div>-->
+	
+
+<div class="container_tips">
+<table cellspacing="0" cellpadding="0">
+	<tr>
+		<td>
+			<img class="tips" src="templates/images/tips_content.png"  alt="tips"/>
+			
+		</td>
+	</tr>
+</table>
+<a TARGET="_blank" href="http://www.amfdc.org/">
+	<img class="abcd" src="templates/images/abcd.png"  alt="abcd"/>
+</a>
+<a TARGET="_blank" href="http://www.lungusa.org/">
+	<img class="lung" src="templates/images/americanlung.png"  alt="lung"/>
+</a>
+<a TARGET="_blank" href="http://www.nationalchildrenscancersociety.org/">
+	<img class="nccs" src="templates/images/nccs.png"  alt="nccs"/>
+</a>
+<a TARGET="_blank" href="http://www.nfb.org/nfb/default.asp">
+	<img class="nfb" src="templates/images/nfb.png"  alt="nfb"/>
+</a>
+<a TARGET="_blank" href="http://www.operationsmile.org/">
+	<img class="operation" src="templates/images/operation.png"  alt="operation"/>
+</a>
+<a TARGET="_blank" href="http://www.cfoa.org/">
+	<img class="cancer" src="templates/images/cancer_fund.png"  alt="cancer"/>
+</a>
+<a TARGET="_blank" href="http://www.teenchallenge.com/">
+	<img class="teens" src="templates/images/teen.png"  alt="teens"/>
+</a>
+<a TARGET="_blank" href="http://www.salvationarmy.org/">
+	<img class="salvation" src="templates/images/salvation.png"  alt="salvation"/>
+</a>
+<a TARGET="_blank" href="http://www.vca.org/">
+	<img class="vans" src="templates/images/vans.png"  alt="vans"/>
+</a>
+<a TARGET="_blank" href="http://www.unitedway.org/">
+	<img class="united" src="templates/images/united.png"  alt="united"/>
+</a>
+<a TARGET="_blank" href="http://www.redcross.org/">
+	<img class="red" src="templates/images/red.png"  alt="red"/>
+</a>
+<a TARGET="_blank" href="http://www.hugsnotdrugs.com/">
+	<img class="hug" src="templates/images/hugs.png"  alt="hug"/>
+</a>
+<a TARGET="_blank" href="http://www.nhautism.org/">
+	<img class="autisim" src="templates/images/autisim.png"  alt="autisim"/>
+</a>
+<a TARGET="_blank" href="http://www.missingkids.com/">
+	<img class="missing" src="templates/images/missing.png"  alt="missing"/>
+</a>
+<a TARGET="_blank" href="http://www.nmss.org/">
+	<img class="ms" src="templates/images/ms.png"  alt="ms"/>
+</a>
+<a TARGET="_blank" href="http://www.childsearch.org/">
+	<img class="child" src="templates/images/child.png"  alt="child"/>
+</a>
+<a TARGET="_blank" href="http://ww.helpforwomen.org/">
+	<img class="hfw" src="templates/images/hfw.png"  alt="hfw"/>
+</a>
+<a TARGET="_blank" href="http://www.diabetes.org/">
+	<img class="diabetes" src="templates/images/diabetes.png"  alt="diabetes"/>
+</a>
+<a TARGET="_blank" href="http://www.aafrc.org/">
+	<img class="living" src="templates/images/living.png"  alt="living"/>
+</a>
+<a TARGET="_blank" href="http://Carolinahairfoundation.org/">
+	<img class="chf" src="templates/images/chf.png"  alt="chf"/>
+</a>
+<a TARGET="_blank" href="http://www.childquest.org/">
+	<img class="quest" src="templates/images/quest.png"  alt="quest"/>
+</a>
+</div>
+<?php //include ("templates/footer.php"); ?>
+
+</body>
+</html>
+
